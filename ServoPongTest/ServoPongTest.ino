24da4260b2d7cc198711d981932b2f3b38548eea
kimlaberinto <kim.laberinto@gmail.com>
2016-12-29 07:46:15 -0600

    Added distance sensing to Pong code. One paddle.

--- ServoPongTest/ServoPongTest.ino
+++ ServoPongTest/ServoPongTest.ino
@@ -4,8 +4,18 @@
 Servo topServo;  // create servo object to control a servo
 Servo botServo;
 
+const float MAX_PADDLE_1 = 22.0;
+const float MIN_PADDLE_1 = 7.0;
+const float PADDLE_1_LENGTH = 5;
+float paddle_1_distance = -1;
+
 int topServoPin = 6;
 int botServoPin = 5;
+
+int trigPin = 13;
+int echoPin = 12;
+const float SPEED_OF_SOUND_INV = 29.14;
+
 
 int laserPin = 9;
 
@@ -23,73 +33,128 @@ float maxDirSpeed = 0.01;
 float backgroundColor = 255;
 
 void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT); 
+
   randomSeed(analogRead(0));
   topServo.attach(topServoPin);  // attaches the servo on pin 9 to the servo object
   botServo.attach(botServoPin);
-  
+
   //Random Initial Position
   tPos = random(MAX_ANGLE_T-5) + 5;
   bPos = random(MAX_ANGLE_B-5) + 5;
-  
+
   //Zero Initial Position
   //tPos = 0;
   //bPos = 0;
-  
+
   //Random Speed
-  tVel = random(maxDirSpeed*2) - maxDirSpeed/2.0;
-  bVel = random(maxDirSpeed*2) - maxDirSpeed/2.0;
-  
+  tVel = min(random(1000)/1000.0*maxDirSpeed - maxDirSpeed/2.0, 0.005);
+  bVel = min(random(1000)/1000.0*maxDirSpeed - maxDirSpeed/2.0, 0.005);
+
   //Speed is zero setting
   //tVel = 0;
   //bVel = 0;
-  
+
   pinMode(laserPin, OUTPUT);
   digitalWrite(laserPin, HIGH);
 }
 
 void loop() {
-  
-//  tPos = 0;
-//  bPos = 0;
-//  topServo.write(tPos); 
-//  botServo.write(bPos);
-//  delay(1000);
-//  tPos = 0;
-//  bPos = MAX_ANGLE_B;
-//  topServo.write(tPos); 
-//  botServo.write(bPos);
-//  delay(1000);
-//  tPos = MAX_ANGLE_T;
-//  bPos = MAX_ANGLE_B;
-//  topServo.write(tPos); 
-//  botServo.write(bPos);
-//  delay(1000);
-//  tPos = MAX_ANGLE_T;
-//  bPos = 0;
-//  topServo.write(tPos); 
-//  botServo.write(bPos);
-//  delay(1000);
+
+  //  tPos = 0;
+  //  bPos = 0;
+  //  topServo.write(tPos); 
+  //  botServo.write(bPos);
+  //  delay(1000);
+  //  tPos = 0;
+  //  bPos = MAX_ANGLE_B;
+  //  topServo.write(tPos); 
+  //  botServo.write(bPos);
+  //  delay(1000);
+  //  tPos = MAX_ANGLE_T;
+  //  bPos = MAX_ANGLE_B;
+  //  topServo.write(tPos); 
+  //  botServo.write(bPos);
+  //  delay(1000);
+  //  tPos = MAX_ANGLE_T;
+  //  bPos = 0;
+  //  topServo.write(tPos); 
+  //  botServo.write(bPos);
+  //  delay(1000);
+
 
   topServo.write(tPos); 
   botServo.write(bPos);
-  
+
   tPos = tPos + tVel;
   bPos = bPos + bVel;
-  
+
   if (tPos <= 0) {
     tVel = -1 * tVel;
     tPos = 0; 
-  } else if (tPos >= MAX_ANGLE_T) {
+  } 
+  else if (tPos >= MAX_ANGLE_T) {
     tVel = -1 * tVel;
     tPos = MAX_ANGLE_T;
   }
-  
+
   if (bPos <= 0) {
-    bVel = -1 * bVel;
-    bPos = 0; 
-  } else if (bPos >= MAX_ANGLE_B) {
+    digitalWrite(trigPin, LOW);
+    delay(2);
+    digitalWrite(trigPin, HIGH);
+
+    delay(10);
+    digitalWrite(trigPin, LOW);
+    float duration = pulseIn(echoPin, HIGH); // [microseconds]
+
+    paddle_1_distance = duration/2 / SPEED_OF_SOUND_INV; //[m] Calculate the dsitance using the speed of sound
+
+    float height = 40.0;
+    float ballDist = height*tan(tPos/180*3.14) + 9.02;
+
+    Serial.print(paddle_1_distance);
+    Serial.print(",");
+    Serial.print(ballDist);
+    Serial.println("");
+
+    if ( paddle_1_distance-1.2 < ballDist && ballDist < (paddle_1_distance + PADDLE_1_LENGTH + 1.2))
+    {     
+      bVel = min(-1.2 * bVel,0.037);
+      tVel = 1.00 * tVel;
+      bPos = 0; 
+      Serial.println("HIT");
+    } 
+    else {
+
+      tPos = MAX_ANGLE_T/2;
+      bPos = MAX_ANGLE_B/2;
+      tVel = 0;
+      bVel = 0;
+      Serial.println("MISSES");
+      topServo.write(tPos); 
+      botServo.write(bPos);
+      delay(5000);
+
+      tPos = MAX_ANGLE_T/2;
+      bPos = MAX_ANGLE_B/2;
+
+
+      randomSeed(analogRead(0));
+      tVel = min(random(1000)/1000.0*maxDirSpeed - maxDirSpeed/2.0, 0.005);
+      bVel = min(random(1000)/1000.0*maxDirSpeed - maxDirSpeed/2.0, 0.005);
+
+    }
+
+  } 
+  else if (bPos >= MAX_ANGLE_B) {
     bVel = -1 * bVel;
     bPos = MAX_ANGLE_B;
   }
 
+
 }
+
+
+
